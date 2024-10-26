4d48a4a1a4591d65375ce6375fcc4c4a5ef1e221
nswierzbinowicz <161207554+nswierzbinowicz@users.noreply.github.com>
2024-10-26 17:15:45 +0200

    Add files via upload

---
+++ AlgorytmyiStrukturyDanych/AlgorytmyiStrukturyDanych/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmyiStrukturyDanych
+{
+    internal class List
+    {
+        public NodeL head;
+        public NodeL tail;
+        public int count;
+
+        public void AddFirst(int liczba)
+        {
+
+        }
+        public void AddLast(int liczba)
+        {
+            var tmp = new NodeL(7);
+            tmp.p = this tail;
+            tmp.n = 
+        }
+    }
+}
